896ad72dff64e563f798ba0da4ce8da419eca67b
steve <873057692@qq.com>
2021-01-04 01:00:12 +0800

    js

---
+++ tools/getBabelCommonConfig.js
@@ -0,0 +1,40 @@
+// babel-plugin-transform-vue-jsx
+// babel-plugin-inline-import-data-uri
+// babel-plugin-transform-es3-member-expression-literals
+// babel-plugin-transform-es3-property-literals
+// babel-plugin-transform-object-assign
+// babel-plugin-transform-object-rest-spread
+// babel-plugin-transform-class-properties
+
+// nev-test babel-plugin-istanbul
+module.exports = function (modules) {
+  const plugins = [
+    require.resolve('babel-plugin-transform-vue-jsx')
+  ]
+  plugins.push([
+    require.resolve('@babel/plugin-transform-runtime')
+  ])
+  console.log(plugins, 'plugins .. ..')
+  return {
+    presets: [
+      [
+        require.resolve('@babel/preset-env'),
+        {
+          modules,
+          targets: {
+            browsers: [
+              'last 2 versions',
+              'Firefox ESR',
+              '> 1%',
+              'ie >= 9',
+              'iOS >= 8',
+              'Android >= 4'
+            ]
+          }
+        }
+      ]
+    ],
+    plugins: plugins,
+    env: {}
+  }
+}
--- tools/gulpfile.js
+++ tools/gulpfile.js
@@ -1,54 +1,89 @@
-const gulp = require('gulp')
-const path = require('path')
+const gulp = require('gulp');
+const path = require('path');
 // const rimraf = require('rimraf')
-const through2 = require('through2')
-const merge2 = require('merge2')
-const {src, series, dest} = gulp
-const transformLess = require('./transformLess')
-const util = require('./utils')
+const through2 = require('through2');
+const merge2 = require('merge2');
+const { src, series, dest } = gulp;
+const transformLess = require('./transformLess');
+const babel = require('gulp-babel');
+const getBabelCommonConfig = require('./getBabelCommonConfig');
+const util = require('./utils');
 
-const cwd = process.cwd()
-const libDir = path.join(cwd, 'lib')
-const esDir = path.join(cwd, 'es')
-const distDir = path.join(cwd, 'dist')
+const cwd = process.cwd();
+const libDir = path.join(cwd, 'lib');
+const esDir = path.join(cwd, 'es');
+const distDir = path.join(cwd, 'dist');
 
-function compile (dir) {
+function compile(dir) {
   // 删除dist目录
-  const sources = ['components/**/*.js', 'components/**/*.jsx', '!components/*/__tests__/*']
-  return merge2([compileLess(dir), compileJsFileStream(gulp.src(sources), dir)])
+  const sources = [
+    'components/**/*.js',
+    'components/**/*.jsx',
+    '!components/*/__tests__/*'
+  ];
+  return merge2([
+    compileLess(dir),
+    compileJsFileStream(gulp.src(sources), dir)
+  ]);
 }
-function compileLess (dir) {
-  return src('components/**/index\.less')
-  .pipe(through2.obj(function (file, encoding, next) {
-    this.push(file.clone())
-    const filePath = util.formatPath(file.path)
-    if (filePath.match(/\/style\/index\.less/)) {
-      transformLess(filePath)
-      .then(css => {
-        file.contents = Buffer.from(css)
-        file.path = filePath.replace(/\.less$/, '.css')
-        this.push(file)
-        next()
+function compileLess(dir) {
+  return src('components/**/index.less')
+    .pipe(
+      through2.obj(function (file, encoding, next) {
+        this.push(file.clone());
+        const filePath = util.formatPath(file.path);
+        if (filePath.match(/\/style\/index\.less/)) {
+          transformLess(filePath)
+            .then((css) => {
+              file.contents = Buffer.from(css);
+              file.path = filePath.replace(/\.less$/, '.css');
+              this.push(file);
+              next();
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        } else {
+          next();
+        }
       })
-      .catch(err => {
-        console.error(err)
-      })
-    } else {
-      next()
-    }
-  }))
-  .pipe(dest(dir))
+    )
+    .pipe(dest(dir));
 }
 
-function compileJsFileStream (js, dir) {
-  const stream = js.pipe(through2.obj(function (file, encoding, next) {
-    this.push(file.clone())
-
-  }))
+function compileJsFileStream(js, dir) {
+  const modules = dir === esDir ? false : void 0;
+  const babelConfig = getBabelCommonConfig(modules);
+  babelConfig.babelrc = false;
+  delete babelConfig.cacheDirectory;
+  if (module === false) {
+    // replaceLib
+  }
+  console.log('before babel....')
+  const stream = js.pipe(babel(babelConfig)).pipe(
+    through2.obj(function (file, encoding, next) {
+      this.push(file.clone());
+      const filePath = util.formatPath(file.path);
+      // if (filePath.match(/\/style\/index\.(js|jsx)$/)) {
+      //   const content = file.contents.toString(encoding)
+      //   file.contents =  Buffer.from(content.replace(/\/style\/?'/g, "/style/css'").replace(/\.less/g, '.css'))
+      //   file.path = file.path.replace(/index\.(js|jsx)$/, 'css.js')
+      //   this.push(file)
+      //   next()
+      // } else {
+      //   next()
+      // }
+      next();
+    })
+  );
+  return stream.pipe(gulp.dest(dir));
 }
 
-console.log(`cwd: ${cwd}/nlib: ${libDir}/nes: ${esDir}`)
+console.log(`cwd: ${cwd}/nlib: ${libDir}/nes: ${esDir}`);
 
-gulp.task('compile-with-es', series(done => {
-  compile(esDir).on('finish', () => done())
-}))
+gulp.task(
+  'compile-with-es',
+  series((done) => {
+    compile(esDir).on('finish', () => done());
+  })
+);
