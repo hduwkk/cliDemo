d54e74ad428aa7153ae2cbcc22db23ef41504308
hduwkk <873057692@qq.com>
2021-01-15 19:53:35 +0800

    ..

--- otherDemo/md.js
+++ otherDemo/md.js
@@ -1,10 +1,45 @@
 const fs = require('fs')
 const path = require('path')
+var hljs = require('highlight.js')
+const md = require('markdown-it')({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str, true).value
+      } catch(err) {
+        console.log(err)
+      }
+    }
+    return ''
+  },
+  html: true
+})
+md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  const result = self.renderToken(tokens, idx, options)
+  console.log('link_open ....', result)
+  return result
+}
 
 // const regPath = `${path.join(__dirname, '../components')}/**/README.md`
 // console.log(regPath, 'regPath')
-console.log(path.join(__dirname, '../components/**/README.md'))
-console.log(path.resolve(__dirname, '../components/**/README.md'))
-const content = fs.readFileSync(path.resolve(__dirname, '../components/**/README.md'))
+// console.log(path.join(__dirname, '../components/**/README.md'))
+// console.log(path.resolve(__dirname, '../components/**/README.md'))
+// const content = fs.readFileSync(path.resolve(__dirname, '../components/**/README.md'))
 
-console.log(content, 'content')
+const baseDir = path.join(__dirname, '../components')
+const dirs = fs.readdirSync(baseDir, {withFileTypes: true})
+dirs.forEach(dir => {
+  if (dir.isDirectory()) {
+    fs.readFile(path.join(baseDir, dir.name, 'README.md'), { encoding: 'utf-8' }, (err, file) => {
+      if (err) return console.log(err.toString())
+      const result = md.render(file.toString())
+      fs.writeFile(dir.name + '_README.html', result, (err) => {
+        if (err) {
+          return console.log(err)
+        }
+        console.log(dir.name + ' 文件已保存')
+      })
+      // console.log(result, 'file')
+    })
+  }
+})
